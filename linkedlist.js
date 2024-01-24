1b63d22dc371c604a2375b83d035a85967b3fc0e
Jarly Love Baaclo <jlbaaclo87@gmail.com>
2024-01-24 13:46:01 +0800

    update the linkedlist.js

--- linkedlist.js
+++ linkedlist.js
@@ -10,6 +10,11 @@ class LinkedList{
         this.head = null;
         this.size=0;
     }
+    //clear list
+    clearList(){
+        this.head = null;
+        this.size=0;
+    }
     //Insert node at start
     insertStart(data){
         this.head = new Node(data,this.head);
@@ -35,8 +40,27 @@ class LinkedList{
     insertPosition(data, index){
         //check if position is out of range
         if(index > 0 && index > this.size){
-
+            return console.log(`slot not available`)
         }
+        if (index === 0) {
+            this.insertStart(data);
+            return;
+        }
+        // otherwise the position is greater than zero
+        const node = new Node(data);
+        let current, previous;
+        // set first to current
+        current = this.head;
+        let count = 0;
+
+        while (count < index) {
+            previous = current;
+            count++;
+            current=current.next;
+        }
+        node.next = current;
+        previous.next = node;
+
     }
 
     //display nodes from the list
@@ -56,6 +80,9 @@ nodeList.insertStart(200);
 nodeList.insertStart(400);
 nodeList.insertEnd(100);
 nodeList.insertPosition(300,1);
+nodeList.insertPosition(150,3);
+nodeList.printLinkedList();
+nodeList.clearList();
 nodeList.printLinkedList();
 
 // const n1 = new Node(100,2);
